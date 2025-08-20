29f52291caf23c6c329dd724216b7c8925ded2e7
Trisha <trisha.a.dejesus@gmail.com>
2025-08-20 17:03:44 -0400

    adding sections

---
+++ src/Components/Featured.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Featured = () => {
+    return (
+       <section id="features">
+        <div className="container">
+            <div className="row">
+                <h2 className="section__title">
+                    Featured <span className="purple">Books</span>
+                </h2>
+            </div>
+        </div>
+       </section>
+    );
+
+}
+
+export default Featured;
