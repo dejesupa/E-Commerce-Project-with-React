3930d37dac0cae6d55012d9fd71fdfbe2e98e7d0
Trisha <trisha.a.dejesus@gmail.com>
2025-08-20 22:50:19 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import Discounted from "./Components/Discounted";
+import Explore from "./Components/Explore";
 import Featured from "./Components/Featured";
+import Footer from "./Components/Footer";
 import Highlights from "./Components/Highlights";
 import Landing from "./Components/Landing";
 import Nav from "./Components/Nav";
@@ -10,6 +13,9 @@ function App() {
     <Landing />
     <Highlights />
     <Featured />
+    <Discounted />
+    <Explore />
+    <Footer />
     </div>
   );
 }
