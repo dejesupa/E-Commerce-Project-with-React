3848ba7a0cb7da33c37630ab1ff896e36ac0eecd
Trisha <trisha.a.dejesus@gmail.com>
2025-08-20 22:01:34 -0400

    added ratings to featured books

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -1,6 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import Book from './ui/Book';
+import { books } from '../data.js'
 
 const Featured = () => {
+    console.log(books);
+    console.log(books.filter(book => book.rating === 5).slice(0, 4));
+    
     return (
        <section id="features">
         <div className="container">
@@ -8,6 +14,14 @@ const Featured = () => {
                 <h2 className="section__title">
                     Featured <span className="purple">Books</span>
                 </h2>
+                <div className="books">
+                    {books
+                        .filter((book) => book.rating === 5)
+                        .slice(0, 4)
+                        .map(book => (
+                        <Book book={book} key={book.id}/>
+                    ))}
+                </div>
             </div>
         </div>
        </section>
