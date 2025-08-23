59f029477c941abd964fd29ca526f504a5331e91
Trisha <trisha.a.dejesus@gmail.com>
2025-08-23 15:54:36 -0400

    bookExistsOnCart

--- src/App.js
+++ src/App.js
@@ -4,19 +4,36 @@ import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from "./data";
 import Footer from "./Components/Footer";
+import Bookinfo from "./pages/Bookinfo";
+import Cart from "./pages/Cart";
+import React, { useEffect, useState } from 'react'
 
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+function addToCart(book) {
+  setCart([...cart, book])
+}
+
+useEffect(() => {
+  console.log(cart)
+}, [cart])
+
+ 
+
   return (
     <Router>
       <div className="App">
         <Nav />
       
          <Routes>
-        <Route path="/" exact component={Home} />
-        <Route path="/books" render={() => <Books books={books} />} />
-        
-              {/* <Route path="/books" element={() => <Books books={books} />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books books={books} />} />
+        <Route path="/books/:id" element={<Bookinfo books={books} addToCart={addToCart} />} />
+        <Route path="/cart" element={<Cart books={books} />} />
+
+       
         {/* <Route path="/books/:id" element={<Bookinfo books={books}
         addToCart={addToCart} cart={cart} />}/>
         <Route path="/cart" element={<Cart books={books} cart={cart}
